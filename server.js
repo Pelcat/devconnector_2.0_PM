a28dffbb157c25fcaba5a1360f7644c805d3eedd
Pelcat <54308320+Pelcat@users.noreply.github.com>
2019-11-20 14:33:05 +1030

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: true }));
+//app.use(express.json()); //Used to parse JSON bodies
+//app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
